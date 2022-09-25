63fb5b91d2a2883e529a8d8101ba763d5cf249f5
Jakub Kozłowski <kubukoz@gmail.com>
2022-09-25 04:53:03 +0200

    scala-cli: use x86_64 fallback on aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -16,10 +16,23 @@
       typelevelShell = ./modules/typelevelShell.nix;
       forSystem = system:
         let
+          scala-cli-override = final: prev:
+            {
+              scala-cli =
+                if system == "aarch64-darwin"
+                then
+                  let x86Packages = import nixpkgs { system = "x86_64-darwin"; }; in
+                  # Using x86_64's scala-cli, but a native JVM for optimal performance
+                    # (only the CLI will be emulated with Rosetta, whereas the bloop server will not)
+                  x86Packages.scala-cli.override { inherit (final) jre; }
+                else prev.scala-cli;
+            };
+
           pkgs = import nixpkgs {
             inherit system;
             overlays = [
               self.overlay
+              scala-cli-override
               (import ./overlay.nix)
             ];
           };
