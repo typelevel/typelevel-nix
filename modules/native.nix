4fd92f494f23997d62dd3f66aceb3b7838ca256b
Ross A. Baker <ross@rossabaker.com>
2022-10-05 13:37:17 -0400

    Reform the scala-native support

---
+++ modules/native.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.typelevelShell.native;
+in
+{
+  options.typelevelShell.native = {
+    enable = mkEnableOption "Provide scala-native environment";
+
+    libraries = mkOption {
+      type = types.listOf types.package;
+      default = [ pkgs.zlib ];
+      description = "A list of split-output libraries to include (dev output) and link (lib output)";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    devshell.packages = [
+      pkgs.clang
+    ] ++ flatten (map (e: [ (getDev e) (getLib e) ]) cfg.libraries);
+
+    env = [
+      {
+        name = "LIBRARY_PATH";
+        prefix = "$DEVSHELL_DIR/lib";
+      }
+      {
+        name = "C_INCLUDE_PATH";
+        prefix = "$DEVSHELL_DIR/include";
+      }
+    ];
+  };
+}
