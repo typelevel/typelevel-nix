6d17beb4ca1a0ced3dd32686be6a8a8eb45cd5f8
Ross A. Baker <ross@rossabaker.com>
2022-10-05 21:50:49 -0400

    Check flake; drops aarch64-linux and i686-linux

--- flake.nix
+++ flake.nix
@@ -14,30 +14,27 @@
   outputs = { self, nixpkgs, flake-utils, devshell, ... }:
     let
       typelevelShell = ./modules/typelevelShell.nix;
+
+      systems = [
+        "aarch64-darwin"
+        # TODO no scala-cli
+        # "aarch64-linux"
+        # TODO jdk17 refers to openjdk-headless-16
+        # "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
       forSystem = system:
         let
-          scala-cli-override = final: prev:
-            {
-              scala-cli =
-                if system == "aarch64-darwin"
-                then
-                  let x86Packages = import nixpkgs { system = "x86_64-darwin"; }; in
-                  # Using x86_64's scala-cli, but a native JVM for optimal performance
-                    # (only the CLI will be emulated with Rosetta, whereas the bloop server will not)
-                  x86Packages.scala-cli.override { inherit (final) jre; }
-                else prev.scala-cli;
-            };
-
           pkgs = import nixpkgs {
             inherit system;
             overlays = [
               self.overlay
-              scala-cli-override
               (import ./overlay.nix)
             ];
           };
-        in
-        {
+
           devShells = {
             library = pkgs.devshell.mkShell {
               imports = [ typelevelShell ];
@@ -52,10 +49,13 @@
               typelevelShell.jdk.package = pkgs.jdk17_headless;
             };
           };
+        in
+        {
+          inherit devShells;
+          checks = devShells;
         };
     in
     {
       inherit (devshell) overlay;
-      inherit typelevelShell;
-    } // flake-utils.lib.eachDefaultSystem forSystem;
+    } // flake-utils.lib.eachSystem systems forSystem;
 }
